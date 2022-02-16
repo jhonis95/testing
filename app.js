b2069e4f10a766bdc242fc8fe22a14d2efa22843
jonatan de oliveira <j.jhonis95@gmail.com>
2022-02-16 09:44:34 -0400

    testing js promises

--- app.js
+++ app.js
@@ -1,34 +1,28 @@
-function isTouching(a,b){
-    const aRect = a.getBoundingClientRect();
-	const bRect = b.getBoundingClientRect();
-    return!(
-        aRect.top + aRect.height < bRect.top ||
-		aRect.top > bRect.top + bRect.height ||
-		aRect.left + aRect.width < bRect.left ||
-		aRect.left > bRect.left + bRect.width
-    );
-}
-const avatar=document.querySelector('#player')
-window.addEventListener('keyup',function(e){//ceaching the key that was press
-    if(e.key==='ArrowDown'||e.key==='Down'){
-        const currTop=extractPos(avatar.style.top);
-        avatar.style.top=`${currTop+50}px`//moving the to Down
-    }else if(e.key==='ArrowUp'||e.key==='Up'){
-        const currTop=extractPos(avatar.style.top);
-        avatar.style.top=`${currTop-50}px`//moving the to up
-    }
-    else if(e.key==='ArrowRight'||e.key==='Right'){
-        const currLeft=extractPos(avatar.style.left);
-        avatar.style.left=`${currLeft+50}px`//moving the to Right
-        avatar.style.transform='scale(1,1)';//flipping the image
-    }else if(e.key==='ArrowLeft'||e.key==='Left'){
-        const currLeft=extractPos(avatar.style.left);
-        avatar.style.left=`${currLeft-50}px`//moving the to Left
-        avatar.style.transform='scale(-1,1)';//flipping the image
-    }
-})
+//testing async functions promises
+const weather = true
+const date    = new Promise(function(resolve, reject) {
+  if (weather) {
+    const dateDetails = {
+      name:     'Cubana Restaurant',
+      location: '55th Street',
+      table:    5
+    };
 
-const extractPos=(pos)=>{//to update the position
-    if(!pos)return 100;//in case the pos is undefine we can get set that as 0
-    return parseInt(pos.slice(0,-2))// eleminate the px of the string and turn the string to a number
-};
+    resolve(dateDetails)
+  } else {
+    reject(new Error('Bad weather, so no Date'))
+  }
+});
+const myDate = function() {
+    date
+      .then(function(done) {
+        console.log('We are going on a date!')
+        console.log(done)
+      })
+      .catch(function(error) {
+          console.log(error.message)
+      })
+  }
+  
+  myDate();
+  
