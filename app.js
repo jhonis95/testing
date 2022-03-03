67aec0a1f5a64e270a84396ab228e6c64ea58719
jonatan de oliveira <j.jhonis95@gmail.com>
2022-03-03 13:44:48 -0400

    Testing XML Http Request

--- app.js
+++ app.js
@@ -1,87 +1,40 @@
-//testing async functions promises
-const weather = true
-const date    = new Promise(function(resolve, reject) {
-  if (weather) {
-    const dateDetails = {
-      name:     'Cubana Restaurant',
-      location: '55th Street',
-      table:    5
-    };
+//testing XMLHttpRequests
+const xmlObject= new XMLHttpRequest();//Making the object
 
-    resolve(dateDetails)
-  } else {
-    reject(new Error('Bad weather, so no Date'))
+xmlObject.addEventListener('load',()=>{ //event to handle if the request is accept
+  console.log("worked");
+  const data= JSON.parse(xmlObject.response);    
+  for(let items of data){
+    console.log(items) 
   }
 });
-const myDate = function() {
-    date
-      .then(function(done) {
-        console.log('We are going on a date!')
-        console.log(done)
-      })
-      .catch(function(error) {
-          console.log(error.message)
-      })
-  }
-  
-  myDate();
-//Chaining Promises
-/*
-const weather = true
-const date    = new Promise(function(resolve, reject) {
-  if (weather) {
-    const dateDetails = {
-      name:     'Cubana Restaurant',
-      location: '55th Street',
-      table:    5
-    };
+xmlObject.addEventListener("error",()=>{
+  console.log("deu ruim")
+})
+xmlObject.open('GET','https://fakestoreapi.com/products/')//where to get the request
+xmlObject.send() //majing the request
 
-    resolve(dateDetails)
-  } else {
-    reject(new Error('Bad weather, so no Date'))
-  }
-});
-const oderUber= function(dateDetails){
-	return new Promise(function(resolve,reject){
-		const message=`Get me an Uber ASAP to ${dateDetails.location},we are going on a date!`;;
-		resolve(message)	
-	})
-}
-const myDate = function() {
-  date
-    .then(orderUber)  //chaining promises(execute two or more asynchronous operations based on the result of preceding promises.)
-    .then(function(done) {
-      console.log(done);
-    })
-    .catch(function(error) {
-      console.log(error.message)
-    })
-}
-
-myDate();
- */
-
-//exemple by mmyself
-const Uber=true
-const callUber= new Promise(function(resolve,reject){
-  if(Uber){
-    const address={
-      location: 'vilamix',
-      price:50,
-      people:3
-    }
-    resolve(address)
-  }else{
-    reject(new Error('didnt pick the call'))
-  }
-});
-const calling=function(){
-  callUber.then(function(address){
-    console.log('got the uber')
-    console.log(address)
-  })
-  callUber.catch(function(refuse){
-    console.log(refuse)
-  })
-}
-calling();
+//Nested XHR HTTP Requests
+// const firstReq = new XMLHttpRequest();
+// firstReq.addEventListener('load', function() {
+// 	console.log('FIRST REQUEST WORKED!!!');
+// 	const data = JSON.parse(this.responseText);
+// 	const filmURL = data.results[0].films[0];
+// 	const filmReq = new XMLHttpRequest();
+// 	filmReq.addEventListener('load', function() {
+// 		console.log('SECOND REQUEST WORKED!!!');
+// 		const filmData = JSON.parse(this.responseText);
+// 		console.log(filmData.title);
+// 	});
+// 	filmReq.addEventListener('error', function(e) {
+// 		console.log('ERROR!!', e);
+// 	});
+// 	filmReq.open('GET', filmURL);
+// 	filmReq.send();
+// });
+// firstReq.addEventListener('error', (e) => {
+// 	console.log('ERROR!!!!!!');
+// });
+// firstReq.open('GET', 'https://swapi.co/api/planets/');
+// firstReq.send();
+// console.log('Request Sent!');
